da0b631711acd8ccde7b8a6e4b25b29d9e0a3280
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-10-06 16:25:12 +0300

    add header content

--- src/components/Page_section/Header/Header.jsx
+++ src/components/Page_section/Header/Header.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import style from './Header.module.css';
+import Logo from '../../images/logo.png'
 
 const Header = () => {
+
+
+  const checkForActive = (active) => {
+    if (active === 1) {
+      return style.link
+    } else {
+      return style.link_active
+    }
+  }
+
+
   return (
-    <div>Header</div>
+    <div className={style.body}>
+      {/* Logo */}
+      <div>
+        <img className={style.logo}src={Logo} alt="logo" />
+      </div>
+
+      {/* Link */}
+      <div className={style.link_block}>
+        <a href="/" className={checkForActive(2)}>About</a>
+        <a href="/" className={checkForActive(1)}>Zoos</a>
+        <a href="/" className={checkForActive(1)}>Map</a>
+        <a href="/" className={checkForActive(1)}>Contact Us</a>
+        <a href="/" className={checkForActive(1)}>Design</a>
+      </div>
+
+
+      {/* Toogle switches */}
+      <div className={style.checkbox_apple}>
+        <input className={style.yep} id="check-apple" type="checkbox"/>
+        <label for="check-apple"></label>
+      </div>
+    </div>
   )
 }
 
