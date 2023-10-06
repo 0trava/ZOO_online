116ca0552eb1f7edccadf1a992a40c2da0a42b68
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-10-06 22:52:50 +0300

    add content Pets in Zoo

--- src/components/Page_section/Pets in zoo/PetsInZoo.jsx
+++ src/components/Page_section/Pets in zoo/PetsInZoo.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import style from './PetsInZoo.module.css'
 
 const PetsInZoo = () => {
   return (
-    <div>PetsInZoo</div>
+    <div className={style.body}>
+      <h2 className={style.title}>Pets in Zoo</h2>
+
+      {/* Slider */}
+
+
+      {/* Button */}
+      <button type='button' className={style.button}>
+          <span className={style.btn_txt}>Choose your favorite</span>
+      </button>
+
+    </div>
   )
 }
 
