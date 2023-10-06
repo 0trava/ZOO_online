e8e6d4756ddcfd463816c78b7bd0080576fafb93
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-10-06 23:55:21 +0300

    add footer

--- src/components/Page_section/Footer/Footer.jsx
+++ src/components/Page_section/Footer/Footer.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import style from './Footer.module.css';
+
+// images
+import logo from '../../images/logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={style.body}>
+      <div>
+        <div>
+          <img className={style.logo} src={logo} alt="logo" />
+
+          <h3>Contact us</h3>
+        </div>
+        
+        <div>
+
+        </div>
+        
+
+      </div>
+      <div>
+        {/* button */}
+        <button type='button' className={style.button}>
+          <span className={style.btn_txt}>donate for volunteers</span>
+        </button>
+      </div>
+
+
+
+
+
+
+    </div>
   )
 }
 
