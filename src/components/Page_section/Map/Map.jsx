d55193cb574405663f7c43df5d4405dcb90da60d
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-10-06 23:40:55 +0300

    add map batton

--- src/components/Page_section/Map/Map.jsx
+++ src/components/Page_section/Map/Map.jsx
@@ -4,11 +4,20 @@ import style from './Map.module.css';
 // images
 import map from '../../images/svg/Map.svg'
 
+// import icons
+import {GoPlay} from 'react-icons/go';
+
 const Map = () => {
   return (
     <div className={style.body}>
       <h2 className={style.title}>Map</h2>
       <img src={map} alt="images map" />
+
+      {/* button */}
+      <button type='button' className={style.button}>
+          <GoPlay className={style.button_span}/>
+          <span class="btn-txt">watch online</span>
+        </button>
     </div>
   )
 }
