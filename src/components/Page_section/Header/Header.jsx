c8cfb715055d799dc1f847c8444f9103060049f0
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-10-08 19:31:55 +0300

    add burger menu

--- src/components/Page_section/Header/Header.jsx
+++ src/components/Page_section/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import style from './Header.module.css';
 
+// Icon
+import { AiOutlineClose } from 'react-icons/ai';
+
 // Images
 import LogoLight from '../../images/logo.png';
 import LogoDark from '../../images/logoDark.png';
-
 import ToggleTheme from 'components/elements/ToggleTeme/ToggleTheme';
 
 const Header = () => {
   const them = localStorage.getItem('theme');
   const [logoTheme, setlogoTheme] = useState(them);
+  const [burgerMenu, setBurgerMenu] = useState(false)
 
   useEffect(() => {
 
@@ -24,6 +27,17 @@ const Header = () => {
     }
   }
 
+  // Modal
+  const openBurgerMenu = () => {
+    setBurgerMenu(true);
+  }
+
+  const closeBurgerMenu = () => {
+    setBurgerMenu(false);
+  }
+
+
+
 
   return (
     <div className={style.body}>
@@ -36,6 +50,8 @@ const Header = () => {
         
       </div>
 
+
+
       {/* Link */}
       <div className={style.link_block}>
         <a href="/" className={checkForActive(2)}>About</a>
@@ -45,9 +61,43 @@ const Header = () => {
         <a href="/" className={checkForActive(1)}>Design</a>
       </div>
 
-
+      <div className={style.right_part}>
       {/* Toogle switches */}
         <ToggleTheme setlogoTheme={setlogoTheme}/>
+      
+      {/* Burger button */}
+      <div className={style.burger_btn} onClick={openBurgerMenu}>
+        {them === "light" ? 
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="21" viewBox="0 0 40 21" fill="none">
+                <line y1="1.5" x2="40" y2="1.5" stroke="#333333" stroke-width="3"/>
+                <line y1="10.5" x2="40" y2="10.5" stroke="#333333" stroke-width="3"/>
+                <line x1="7" y1="19.5" x2="40" y2="19.5" stroke="#333333" stroke-width="3"/>
+                </svg>
+              : <svg xmlns="http://www.w3.org/2000/svg" width="40" height="21" viewBox="0 0 40 21" fill="none">
+                <line y1="1.5" x2="40" y2="1.5" stroke="#FEFEFE" stroke-width="3"/>
+                <line y1="10.5" x2="40" y2="10.5" stroke="#FEFEFE" stroke-width="3"/>
+                <line x1="7" y1="19.5" x2="40" y2="19.5" stroke="#FEFEFE" stroke-width="3"/>
+                </svg>
+          }
+      </div>
+      </div>
+
+
+      {/*Burger menu  */}
+      {burgerMenu ? 
+            <div className={style.bacround_modal} onClick={closeBurgerMenu}>
+            <div className={style.burger_menu}>
+              <AiOutlineClose className={style.burger_btn_close} onClick={closeBurgerMenu}/>
+              <a href="/" className={checkForActive(2)}>About</a>
+              <a href="/" className={checkForActive(1)}>Zoos</a>
+              <a href="/" className={checkForActive(1)}>Map</a>
+              <a href="/" className={checkForActive(1)}>Contact Us</a>
+              <a href="/" className={checkForActive(1)}>Design</a>
+            </div>
+            </div>
+        : ""
+      }
+
     </div>
   )
 }
--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Routes = () => {
   return (
-    <div>Routes</div>
+    <>
+
+
+
+    </>
   )
 }
 
